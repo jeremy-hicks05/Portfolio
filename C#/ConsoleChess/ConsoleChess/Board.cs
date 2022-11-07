309a9d5bd46db335394fc87251d22481d9a8345a
jeremy-hicks05 <jeremy-hicks05@users.noreply.github.com>
2022-11-07 16:44:21 -0500

    add functionality to ask for user to input latitude and longitude of piece to be moved, and where to

--- C#/ConsoleChess/ConsoleChess/Board.cs
+++ C#/ConsoleChess/ConsoleChess/Board.cs
@@ -6,48 +6,75 @@ using System.Threading.Tasks;
 
 namespace ConsoleChess
 {
-    public class Board
+    internal class Board
     {
-        static List<Space> _spaces;
+        public Space[][] spaces = new Space[8][];
 
         public Board()
         {
-            _spaces = new List<Space>
+            for (int i = 0; i < 8; i++)
             {
-                new Space
+                spaces[i] = new Space[8];
+                for (int j = 0; j < 8; j++)
                 {
-                    Latitude=0,
-                    Longitude=0,
-                    piece = new Piece(name: "r")
+                    spaces[i][j] = new Space();
                 }
-            };
+            }
+
+            for (int i = 0; i < 8; i ++)
+            {
+                for(int j = 0; j < 8; j ++)
+                {
+                    Space space = new Space();
+                    space.Latitude = i;
+                    space.Longitude = j;
+                    space.piece = new Piece(name: "");
+                    spaces[i][j] = space;
+                }
+            }
+
+            spaces[0][0].piece.Name = "r";
+            spaces[0][1].piece.Name = "n";
+            spaces[0][2].piece.Name = "b";
+            spaces[0][3].piece.Name = "k";
+            spaces[0][4].piece.Name = "q";
+            spaces[0][5].piece.Name = "b";
+            spaces[0][6].piece.Name = "n";
+            spaces[0][7].piece.Name = "r";
+
+            for (int i = 0; i < 8; i++)
+            {
+                spaces[1][i].piece.Name = "p";
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                spaces[6][i].piece.Name = "P";
+            }
+
+            spaces[7][0].piece.Name = "R";
+            spaces[7][1].piece.Name = "N";
+            spaces[7][2].piece.Name = "B";
+            spaces[7][3].piece.Name = "K";
+            spaces[7][4].piece.Name = "Q";
+            spaces[7][5].piece.Name = "B";
+            spaces[7][6].piece.Name = "N";
+            spaces[7][7].piece.Name = "R";
 
             PrintBoard();
         }
 
         public void PrintBoard()
         {
-            foreach(Space s in _spaces)
+
+            for (int i = 0; i < 8; i++)
             {
-                Console.WriteLine(s.piece.Name);
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(spaces[i][j].piece.Name);
+                }
+                Console.WriteLine();
             }
-            //Console.WriteLine("_________________________");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("+--+--+--+--+--+--+--+--+");
-            //Console.WriteLine("|  |  |  |  |  |  |  |  |");
-            //Console.WriteLine("-------------------------");
         }
     }
 }
--- C#/ConsoleChess/ConsoleChess/Program.cs
+++ C#/ConsoleChess/ConsoleChess/Program.cs
@@ -6,6 +6,62 @@
         {
             Console.WriteLine("Welcome to Chess!");
             Board myBoard = new Board();
+
+            System.ConsoleKey playing = ConsoleKey.Y;
+            while (playing == ConsoleKey.Y)
+            {
+                int startLat;
+                int endLat;
+                int startLong;
+                int endLong;
+
+                Console.Write("Enter Lat and Long for Piece to be moved:");
+                string stLat = Console.ReadLine();
+                if (Int32.TryParse(stLat, out startLat))
+                {
+                    Console.WriteLine("Accepted input");
+                    string stLong = Console.ReadLine();
+                    if (Int32.TryParse(stLong, out startLong))
+                    {
+                        Console.WriteLine("Accepted input");
+                        Console.Write("Enter Lat and Long for destination Space:");
+                        string enLat = Console.ReadLine();
+                        if (Int32.TryParse(enLat, out endLat))
+                        {
+                            Console.WriteLine("Accepted input");
+                            string enLong = Console.ReadLine();
+                            if (Int32.TryParse(enLong, out endLong))
+                            {
+                                Console.WriteLine("Accepted input");
+                                // move piece from startLat/startLong to endLat/endLong
+                                myBoard.spaces[endLat][endLong].piece.Name =
+                                    myBoard.spaces[startLat][startLong].piece.Name;
+
+                                myBoard.spaces[startLat][startLong].piece.Name = " ";
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Try again");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                }
+
+                Console.Clear();
+
+                myBoard.PrintBoard();
+
+                Console.Write("Keep playing?  Y or N:");
+                playing = Console.ReadKey().Key;
+            }
         }
     }
 }
