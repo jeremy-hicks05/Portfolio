9f76941f11cab4aabba69c487722c963a644741b
jeremy-hicks05 <jeremy-hicks05@users.noreply.github.com>
2022-11-23 14:38:40 -0500

    stop bishop from moving down and right if pieces block it - prevents capturing atm

--- C#/ConsoleChess/ConsoleChess/Pieces/Bishop.cs
+++ C#/ConsoleChess/ConsoleChess/Pieces/Bishop.cs
@@ -31,6 +31,18 @@ namespace ConsoleChess.Pieces
 
             if((Math.Abs(fromSpace.X - toSpace.X)) / (Math.Abs(fromSpace.Y - toSpace.Y)) == 1)
             {
+                // from toSpace up to fromSpace - if any spaces contain pieces, return false
+                // if toSpace is down and right
+                if (fromSpace.X < toSpace.X && fromSpace.Y < toSpace.Y)
+                {
+                    for (int i = toSpace.X, k = toSpace.Y; i > fromSpace.X && k > fromSpace.Y; i--, k--)
+                    {
+                        if (Board.spaces[i][k].Piece.belongsToPlayer != Player.None)
+                        {
+                            return false;
+                        }
+                    }
+                }
                 return true;
             }
             return false;
