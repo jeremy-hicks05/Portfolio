5139b2dda34dfc503b8b1bb4205acaf2efe44752
jeremy-hicks05 <jeremy-hicks05@users.noreply.github.com>
2022-12-10 21:24:48 -0500

    add label and description to Program.cs in comments

--- C#/ConsoleChess/ConsoleChessV3/Program.cs
+++ C#/ConsoleChess/ConsoleChessV3/Program.cs
@@ -1,11 +1,34 @@
-﻿/********************************************
- *     ** Console Chess **                  *
- *      by Jeremy Hicks (c) 2022            *
+﻿/* * * * * * * * * * * * * * * * * * * * * * 
+ *       ____________________________       *
+ *      |__..~~ Console Chess ~~..__ |      *
+ *      |----------------------------|      *
+ *      |  by Jeremy Hicks (c) 2022  |      *
+ *      [____________________________]      *
+ *                                          *
  *      Tested By: Kari Seitz               *
  *      Early Version Review: Shaun Lake    *
  *                                          *
  *                                          *
- ********************************************/
+ * * * * * * * * * * * * * * * * * * * * * */
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Description:                                                 *
+ *      Imitates a Chess board, allowing user to input          *
+ *      a letter and number combination to select a space       *
+ *      on the board, and another space to move that space's    *
+ *      piece to.                                               *
+ *                                                              *
+ *      Allows for takebacks and records the history of         *
+ *      moves made.                                             *
+ *      Allows for viewing list of moves made for the           *
+ *      duration of the game.                                   *
+ *                                                              *
+ *      Checks for checkmate and stalemate.                     *
+ *      Follows all the rules of chess.                         *
+ *                                                              *
+ *                                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
 namespace ConsoleChessV3
