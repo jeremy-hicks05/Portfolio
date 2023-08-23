55b57395adc2c4229abe30a0626e9d4bf27f1b51
Jeremy Hicks <jhicks@mtaflint.onmicrosoft.com>
2023-08-23 14:41:58 -0400

    change to scss

--- Angular/ASPAndAngular/WorldCities/WorldCities/src/app/app.module.ts
+++ Angular/ASPAndAngular/WorldCities/WorldCities/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -17,7 +21,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
