9a14ee535b5489aade6094d6ab8087fa65d0b07e
Jeremy Hicks <jhicks@mtaflint.onmicrosoft.com>
2023-08-25 12:01:04 -0400

    add epplus and move excel file

---
+++ Angular/ASPAndAngular/WorldCities/WorldCitiesAPI/Controllers/SeedController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldCitiesAPI.Data;
+
+namespace WorldCitiesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SeedController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
