551d1b9a805e8650169b434a64fda5fa72737b0a
jeremy-hicks05 <49693959+jeremy-hicks05@users.noreply.github.com>
2023-09-15 07:14:42 -0400

    Delete C#/Games/WebChess/MVCChess directory

--- C#/Games/WebChess/MVCChess/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVCChess.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
