57d65deaf50a44ec74774fe789d689c532340723
MTA-Jeremy-Hicks <jhicks@mtaflint.org>
2023-10-11 16:00:46 -0400

    end of chapter 2

--- Python/Ch01/srv/my-project/MyProject/MyProject/MyProject.py
+++ Python/Ch01/srv/my-project/MyProject/MyProject/MyProject.py
@@ -1,23 +1,16 @@
-# bike.py
-# let's define the class Bike
-class Bike:
-    
-    def __init__(self, color, frame_material):
-        self.color=color
-        self.frame_material = frame_material
-        
-    def brake(self):
-        print("Braking!")
-        
-# Let's create a couple of instances
-red_bike = Bike('Red', 'Carbon fiber')
-blue_bike = Bike('Blue', 'Steel')
+class Person:
+        def __init(self, age):
+                self.age = age
+                
+fab = Person()
+fab.age = 42
+print(fab.age)
 
-# Let's inspect the objects we have, instance of the Bike class.
-print(red_bike.color)
-print(red_bike.frame_material)
-print(blue_bike.color)
-print(blue_bike.frame_material)
+print(id(fab))
 
-# let's brake!
-red_bike.brake()
+print(id(fab.age))
+
+fab.age = 25
+
+print(id(fab))
+print(id(fab.age))
